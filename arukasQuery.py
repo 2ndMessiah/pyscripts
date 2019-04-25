c7b9d1801c540505f83f15d10be5e4d7178b22d1
2ndMessiah <lym.0072003@gmail.com>
2019-04-25 14:10:19 +0800

    Update arukasQuery.py

--- arukasQuery.py
+++ arukasQuery.py
@@ -1,10 +1,21 @@
 # coding=utf-8
+''' 
+Arukas will randomly restart the apps that exceed the usage limitations set for free accounts, after which the app's service port and ip will be changed.
+This script helps to retrieve arukas app's ip and port, and forward to serverChan.
+Since app.arukas.io is blocked, one might as deploy this in a VPS and use cron or any scheduled tasking mechanism to poll the info.
+Fill in the parameters in the header first.
+'''
+
 import requests as r
 import os
+
+#parameters 
 apiID=apiID
 apikey=apikey
 portorder=-1
 serverchanaddr=serverchanaddr
+
+
 
 while True:
     try:
